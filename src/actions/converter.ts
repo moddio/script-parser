3100ddcbfaf7d81e59dda3a17cf7d152b5e5a86e
moe-moe-pupil <dhtmitmi@gmail.com>
2023-10-18 00:27:52 +0800

    feat: add converter for actions.json

---
+++ src/actions/converter.ts
@@ -0,0 +1,32 @@
+import jsonFile from 'jsonfile'
+jsonFile.readFile('./actions.json', (err, obj) => {
+  if (err !== null) {
+    console.error(err)
+  }
+  const actionsObj: any = {}
+  Object.values(obj).map((v, idx) => {
+    const value = v as any
+    const actionObj: any = {
+      function: value.key
+    }
+    let length = 0
+    value.data.fragments.map((frag: { type: string, field: string | undefined }, index: any) => {
+      if (frag.type === 'variable' && (frag.field !== undefined)) {
+        actionObj[frag.field] = String.fromCharCode(length + 97)
+        length += 1
+      }
+    })
+    let str = ''
+    for (let i = 0; i <= length; i++) {
+      str += String.fromCharCode(i + 97) + '#'
+    }
+    str += `{return ${JSON.stringify(actionObj)}}`
+    let count = 0
+    actionsObj[value.key] = str.replace(/"/g, function (match) {
+      count++
+      return (count > 4) ? '' : match
+    })
+  })
+  jsonFile.writeFileSync('./converted_actions.json', actionsObj)
+  console.log(JSON.stringify(actionsObj))
+})
