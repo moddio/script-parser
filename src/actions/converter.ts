1756cc565f63821d4b208d1aa07b0fa43b15125e
moe-moe-pupil <dhtmitmi@gmail.com>
2023-11-08 22:21:29 +0800

    fix: use the latest actions.json from api, bump version to 0.0.7
    fix: one more extra function parameter in converted_actions

--- src/actions/converter.ts
+++ src/actions/converter.ts
@@ -1,35 +1,37 @@
 import jsonFile from 'jsonfile'
+import axios from 'axios'
 
-jsonFile.readFile('./src/actions/actions.json', (err, obj) => {
-  if (err !== null) {
-    console.error(err)
-  }
-  const actionsObj: any = {}
-  const keywordsArr: string[] = []
-  Object.values(obj).map((v, idx) => {
-    const value = v as any
-    const actionObj: any = {
-      function: value.key
-    }
-    let length = 0
-    value.data.fragments.map((frag: { type: string, field: string | undefined }, index: any) => {
-      if (frag.type === 'variable' && (frag.field !== undefined)) {
-        actionObj[frag.field] = String.fromCharCode(length + 97)
-        length += 1
+axios.get('https://www.modd.io/api/editor-api/?game=two-houses')
+  .then((res) => {
+    const actionsObj: any = {}
+    const keywordsArr: string[] = []
+    const obj = res.data.message
+    Object.values(obj).map((v: any, idx) => {
+      const value = v
+      const actionObj: any = {
+        function: value.key
       }
+      let length = 0
+      value.data?.fragments.map((frag: { type: string, field: string | undefined }, index: any) => {
+        if (frag.type === 'variable' && (frag.field !== undefined)) {
+          actionObj[frag.field] = String.fromCharCode(length + 97)
+          length += 1
+        }
+      })
+      let str = ''
+      for (let i = 0; i < length; i++) {
+        str += String.fromCharCode(i + 97) + '#'
+      }
+      str += `{return ${JSON.stringify(actionObj)}}`
+      let count = 0
+      actionsObj[value.key] = str.replace(/"/g, function (match) {
+        count++
+        return (count > 4) ? '' : match
+      })
+      keywordsArr.push(value.key)
     })
-    let str = ''
-    for (let i = 0; i <= length; i++) {
-      str += String.fromCharCode(i + 97) + '#'
-    }
-    str += `{return ${JSON.stringify(actionObj)}}`
-    let count = 0
-    actionsObj[value.key] = str.replace(/"/g, function (match) {
-      count++
-      return (count > 4) ? '' : match
-    })
-    keywordsArr.push(value.key)
+    jsonFile.writeFileSync('./src/actions/keywords.json', keywordsArr)
+    jsonFile.writeFileSync('./src/actions/converted_actions.json', actionsObj)
+  }).catch((e) => {
+    console.log(e)
   })
-  jsonFile.writeFileSync('./src/actions/keywords.json', keywordsArr)
-  jsonFile.writeFileSync('./src/actions/converted_actions.json', actionsObj)
-})
